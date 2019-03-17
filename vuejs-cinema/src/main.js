25366fb2f6f81061b7b6a779dc6c7566745b501f
aruyamam <aruyamam@gmail.com>
2019-03-17 12:59:33 +0900

    Setting up components

--- vuejs-cinema/src/main.js
+++ vuejs-cinema/src/main.js
@@ -1,9 +1,48 @@
 import Vue from 'vue';
 import './style.scss';
 
+import genres from './util/genres';
+
+console.log(genres);
+
 const app = new Vue({
-   data: {
-      msg: 'Hello World',
+   components: {
+      'movie-list': {
+         template: `
+            <div id="movie-list">
+               <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+            </div>
+         `,
+         data() {
+            return {
+               movies: [
+                  { title: 'Pulp Fiction' },
+                  { title: 'Home Alone' },
+                  { title: 'Autstin Powers' },
+               ],
+            };
+         },
+      },
+      'movie-filter': {
+         data() {
+            return { genres };
+         },
+         template: `
+            <div id="movie-filter">
+               <h2>Filter results</h2>
+               <div className="filter-group">
+                  <check-filter v-for="genre in genres"></check-filter>
+               </div>
+            </div>
+         `,
+         components: {
+            'check-filter': {
+               template: `
+                  <div>Filter</div>
+               `,
+            },
+         },
+      },
    },
 });
 
---
+++ vuejs-cinema/src/util/genres.js
@@ -0,0 +1,9 @@
+export default Object.freeze({
+   ANIMATION: 'Animation',
+   COMECY: 'Comedy',
+   CRIME: 'Crime',
+   DOCUMENTARY: 'Documentary',
+   DRAMA: 'Drama',
+   FANTASY: 'Fantasy',
+   HORROR: 'Horror',
+});
