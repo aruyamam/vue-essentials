d6c3740104b6acbf46b305e7b0bea287bd2c032b
aruyamam <aruyamam@gmail.com>
2019-03-23 12:22:15 +0900

    Displaying session times

--- vuejs-cinema/src/components/MovieList.vue
+++ vuejs-cinema/src/components/MovieList.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="movie-list">
     <div v-if="filteredMovies.length">
-      <movie-item v-for="movie in filteredMovies" v-bind:movie="movie.movie"></movie-item>
+      <movie-item
+        v-bind:key="movie.id"
+        v-for="movie in filteredMovies"
+        v-bind:movie="movie.movie"
+        v-bind:sessions="movie.sessions"
+        v-bind:day="day"
+      ></movie-item>
     </div>
     <div v-else-if="movies.length" class="no-results">No results.</div>
     <div v-else class="no-results">Loading...</div>
@@ -12,7 +18,7 @@ import genres from "../util/genres";
 import MovieItem from "./MovieItem.vue";
 
 export default {
-  props: ["genre", "time", "movies"],
+  props: ["genre", "time", "movies", "day"],
   methods: {
     moviePassesGenreFilter(movie) {
       if (!this.genre.length) {
--- vuejs-cinema/src/main.js
+++ vuejs-cinema/src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue';
 import './style.scss';
 
 import VueResource from 'vue-resource';
+import moment from 'moment-timezone';
 
 import MovieList from './components/MovieList.vue';
 import MovieFilter from './components/MovieFilter.vue';
 
+moment.tz.setDefault('UTC');
+
 Vue.use(VueResource);
+Object.defineProperty(Vue.prototype, '$moment', {
+   get() {
+      return this.$root.moment;
+   },
+});
 
 const app = new Vue({
    components: {
@@ -17,6 +25,7 @@ const app = new Vue({
       genre: [],
       time: [],
       movies: [],
+      moment,
    },
    created() {
       this.$http.get('/api').then((response) => {
