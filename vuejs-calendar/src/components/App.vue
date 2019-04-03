c55233312a24e8409c57369b9bf469438454be4b
aruyamam <aruyamam@gmail.com>
2019-04-03 11:34:49 +0900

    Generate list of days

--- vuejs-calendar/src/components/App.vue
+++ vuejs-calendar/src/components/App.vue
@@ -1,10 +1,69 @@
 <template>
-  <div>{{ msg }}</div>
+  <div>
+    <div v-bind:key="week.toString()" v-for="week in weeks">
+      Week
+      <div v-bind:key="day.toString()" v-for="day in week">{{ day }}</div>
+    </div>
+  </div>
 </template>
 <script>
 export default {
   data() {
-    return { msg: "Hello World" };
+    return {
+      month: 4,
+      year: 2019
+    };
+  },
+  computed: {
+    days() {
+      // Generating all days in current month
+      const days = [];
+      let currentDay = this.$moment(`${this.year}-${this.month}-1`, "YYYY-M-D");
+
+      do {
+        days.push(currentDay);
+        currentDay = this.$moment(currentDay).add(1, "days");
+      } while (currentDay.month() + 1 === this.month);
+
+      // Add previous days to start
+      currentDay = this.$moment(days[0]);
+
+      const SUNDAY = 0;
+      const MONDAY = 1;
+
+      if (currentDay.day() !== MONDAY) {
+        do {
+          currentDay = this.$moment(currentDay).subtract(1, "days");
+          days.unshift(currentDay);
+        } while (currentDay.day() !== MONDAY);
+      }
+
+      // Add following days to end
+      currentDay = this.$moment(days[days.length - 1]);
+
+      if (currentDay.day() !== SUNDAY) {
+        do {
+          currentDay = this.$moment(currentDay).add(1, "days");
+          days.push(currentDay);
+        } while (currentDay.day() !== SUNDAY);
+      }
+
+      return days;
+    },
+    weeks() {
+      const weeks = [];
+      let week = [];
+
+      for (const day of this.days) {
+        week.push(day);
+        if (week.length === 7) {
+          weeks.push(week);
+          week = [];
+        }
+      }
+
+      return weeks;
+    }
   }
 };
 </script>
--- vuejs-calendar/src/web.entry.js
+++ vuejs-calendar/src/web.entry.js
@@ -1,11 +1,23 @@
 import Vue from 'vue';
 import './style.scss';
 
+import moment from 'moment-timezone';
+
 import App from './components/App.vue';
+
+moment.tz.setDefault('UTC');
+Object.defineProperty(Vue.prototype, '$moment', {
+   get() {
+      return this.$root.moment;
+   },
+});
 
 const app = new Vue({
    components: {
       App,
+   },
+   data: {
+      moment,
    },
 });
 
