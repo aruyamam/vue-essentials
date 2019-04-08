b2bf1657475fb61234e9be147abc0747bd3e5355
aruyamam <aruyamam@gmail.com>
2019-04-08 11:38:39 +0900

    Displaying events in calendar-day

--- vuejs-calendar/src/components/CalendarDay.vue
+++ vuejs-calendar/src/components/CalendarDay.vue
@@ -1,10 +1,33 @@
 <template>
-  <div :class="classObject" @click="captureClick">{{ day.format('D') }}</div>
+  <div :class="classObject" @click="captureClick">
+    {{ day.format('D') }}
+    <ul class="event-list">
+      <li v-for="event in events">{{ event.description }}</li>
+    </ul>
+  </div>
 </template>
 <script>
 export default {
   props: ["day"],
   computed: {
+    events() {
+      let mockData = [
+        {
+          description: "Random event 1",
+          date: this.$moment("2019-04-06", "YYYY-MM-DD")
+        },
+        {
+          description: "Random event 2",
+          date: this.$moment("2019-04-15", "YYYY-MM-DD")
+        },
+        {
+          description: "Random event 3",
+          date: this.$moment("2019-05-02", "YYYY-MM-DD")
+        }
+      ];
+
+      return mockData.filter(event => event.date.isSame(this.day, "day"));
+    },
     classObject() {
       let today = this.day.isSame(this.$moment(), "day");
 
